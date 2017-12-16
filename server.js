ee7db42611c3ae672bb486682eec2ba2ea7bbc61
nateshmbhat <nateshmbhat1@gmail.com>
2017-12-16 20:47:48 +0530

    added server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+var fs = require("fs") ; 
+var http = require("http") ;
 
+var server = http.createServer(function(req , res){
+
+    res.writeHead(200 , {'Content-Type' : 'text/html'}); 
+    
+    // fs.readFile("../Attention_Please/src/index.html", function(err , data)
+    // {
+    //     res.write(data) ;        
+    // }) ;
+
+    res.write("<h1>Hello this is my resp</h1>") ;
+
+    res.end() ;
+
+})
+
+
+server.listen(8000 , "127.0.0.1") ;
+console.log("Listening to port 8000") ;
