df7c9d69c49036557395a33a794a3095e9adcc7f
nateshmbhat <nateshmbhat1@gmail.com>
2017-12-19 19:14:02 +0530

    updated login page with proper styles and positioning

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ var http = require("http") ;
 var url = require("url") ; 
 var qs = require("querystring") ; 
 var app = require("express")() ; 
+var express = require("express") ;
+
+
+app.use('/views/css' , express.static('views/css' )  ) ;
 
 app.get("/" , (req , res)=>{
     console.log(req.url) ;
     res.sendfile("./index.html") ; 
 }) ; 
+
 
 app.get("/login" , (req , res)=>{
     res.sendfile("./views/login.html") ;
