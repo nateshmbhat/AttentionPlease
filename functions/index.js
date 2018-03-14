0a1fc8bc0ef9d86bba65f8ad5039c529389084d0
KPS <karanpratapsinghgem@gmail.com>
2018-03-14 19:06:07 +0530

    Excel data extrction successful-KPS

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,23 @@
 /// <reference path=".\node_modules\@types\express\index.d.ts" />
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   ALL IMPORTS 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   ALL IMPORTS
 
 const functions = require('firebase-functions');
-const app = require("express")() ; 
+const app = require("express")() ;
 const  express = require("express") ;
 const handle_requests = require("./requests_controller") ;
-const cookieparser = require("cookie-parser") ; 
+const cookieparser = require("cookie-parser") ;
+var xlsx=require('xlsx');
 
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   END OF IMPORTS 
 
-app.use(cookieparser()) ; 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   END OF IMPORTS
+
+app.use(cookieparser()) ;
 app.use(express.static('../public' )  ) ;
+var obj=xlsx.readFile('views/test.xlsx');
+var sh=obj.SheetNames;
+var dat=xlsx.utils.sheet_to_json(obj.Sheets[sh[0]]);
+console.log(dat);
 
 handle_requests(app) ;
 
